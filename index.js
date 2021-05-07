35eea3431b50d0c0a727f371115968ed902f4c3a
Bharath <bharathalapati23@gmail.com>
2021-05-07 14:32:25 +0530

    fifth commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/posts', postRoutes)
 
 
 const CONNECTION_URL = 'mongodb+srv://manipalRentals:mkQmu6yBScBqDOFa@cluster0.gz5zj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, ()=> {console.log(`Server running on port ${PORT}`)}))
